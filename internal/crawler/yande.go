7c173c35ccd23079f3a5a072fb62366d3d54121b
TyrEamon <yuanbin200409@gmail.com>
2025-12-12 11:18:36 +0800

    更新 yande.go

--- internal/crawler/yande.go
+++ internal/crawler/yande.go
@@ -1,78 +1,77 @@
 package crawler
 
 import (
-\t"context"
-\t"encoding/json"
-\t"fmt"
-\t"log"
-\t"my-bot-go/internal/config"
-\t"my-bot-go/internal/database"
-\t"my-bot-go/internal/telegram"
-\t"strings"
-\t"time"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"my-bot-go/internal/config"
+	"my-bot-go/internal/database"
+	"my-bot-go/internal/telegram"
+	"strings"
+	"time"
 
-\t"github.com/go-resty/resty/v2"
+	"github.com/go-resty/resty/v2"
 )
 
 type YandePost struct {
-\tID        int    `json:"id"`
-\tSampleURL string `json:"sample_url"`
-\tFileURL   string `json:"file_url"`
-\tTags      string `json:"tags"`
+	ID        int    `json:"id"`
+	SampleURL string `json:"sample_url"`
+	FileURL   string `json:"file_url"`
+	Tags      string `json:"tags"`
 }
 
 func StartYande(ctx context.Context, cfg *config.Config, db *database.D1Client, bot *telegram.BotHandler) {
-\tclient := resty.New()
-\t
-\tfor {
-\t\tselect {
-\t\tcase <-ctx.Done():
-\t\t\treturn
-\t\tdefault:
-\t\t\tlog.Println("🔍 Checking Yande...")
-\t\t\turl := fmt.Sprintf("https://yande.re/post.json?limit=%d&tags=%s", cfg.YandeLimit, cfg.YandeTags)
-\t\t\t
-\t\t\tresp, err := client.R().Get(url)
-\t\t\tif err != nil {
-\t\t\t\tlog.Printf("Yande Error: %v", err)
-\t\t\t\ttime.Sleep(1 * time.Minute)
-\t\t\t\tcontinue
-\t\t\t}
+	client := resty.New()
+	
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			log.Println("🔍 Checking Yande...")
+			url := fmt.Sprintf("https://yande.re/post.json?limit=%d&tags=%s", cfg.YandeLimit, cfg.YandeTags)
+			
+			resp, err := client.R().Get(url)
+			if err != nil {
+				log.Printf("Yande Error: %v", err)
+				time.Sleep(1 * time.Minute)
+				continue
+			}
 
-\t\t\tvar posts []YandePost
-\t\t\tif err := json.Unmarshal(resp.Body(), &posts); err != nil {
-\t\t\t\tlog.Printf("Yande JSON Error: %v", err)
-\t\t\t\tcontinue
-\t\t\t}
+			var posts []YandePost
+			if err := json.Unmarshal(resp.Body(), &posts); err != nil {
+				log.Printf("Yande JSON Error: %v", err)
+				continue
+			}
 
-\t\t\tfor _, post := range posts {
-\t\t\t\tpid := fmt.Sprintf("yande_%d", post.ID)
-\t\t\t\t
-\t\t\t\t// 简单的内存去重检查 (可选)
-\t\t\t\tif db.History[pid] {
-\t\t\t\t\tcontinue
-\t\t\t\t}
+			for _, post := range posts {
+				pid := fmt.Sprintf("yande_%d", post.ID)
+				
+				// 简单的内存去重检查 (可选)
+				if db.History[pid] {
+					continue
+				}
 
-\t\t\t\timgURL := post.SampleURL
-\t\t\t\tif imgURL == "" {
-\t\t\t\t\timgURL = post.FileURL
-\t\t\t\t}
+				imgURL := post.SampleURL
+				if imgURL == "" {
+					imgURL = post.FileURL
+				}
 
-\t\t\t\tlog.Printf("Downloading Yande: %d", post.ID)
-\t\t\t\timgResp, err := client.R().Get(imgURL)
-\t\t\t\tif err != nil {
-\t\t\t\t\tcontinue
-\t\t\t\t}
+				log.Printf("Downloading Yande: %d", post.ID)
+				imgResp, err := client.R().Get(imgURL)
+				if err != nil {
+					continue
+				}
 
-\t\t\t\tcaption := fmt.Sprintf("Yande: %d
-Tags: #%s", post.ID, strings.ReplaceAll(post.Tags, " ", " #"))
-\t\t\t\t
-\t\t\t\tbot.ProcessAndSend(ctx, imgResp.Body(), pid, post.Tags, caption, "yande")
-\t\t\t\ttime.Sleep(2 * time.Second)
-\t\t\t}
+				caption := fmt.Sprintf("Yande: %d\nTags: #%s", post.ID, strings.ReplaceAll(post.Tags, " ", " #"))
+				
+				bot.ProcessAndSend(ctx, imgResp.Body(), pid, post.Tags, caption, "yande")
+				time.Sleep(2 * time.Second)
+			}
 
-\t\t\tlog.Println("😴 Yande Done. Sleeping 10m...")
-\t\t\ttime.Sleep(10 * time.Minute)
-\t\t}
-\t}
+			log.Println("😴 Yande Done. Sleeping 10m...")
+			time.Sleep(10 * time.Minute)
+		}
+	}
 }
